f28b5863f34bfdec8fd0e286f440f77e4cd2fc67
sugamsharma30 <168557566+sugamsharma30@users.noreply.github.com>
2024-11-19 17:51:55 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const acount =  123445 // these constants which cant be changed
+let email = "sugam@.com"
+var pas = "23456"
+city = "jaipur"
+let state // it not a good way to declare variable but it will also run    
+// acount = 2 it is not allowed the variables declare after const cant be changed 
+ 
+email = "hahgr@acount.com"
+pas = "3456"
+city = "delhi"
+console.table([email , pas , city ])
+/*
+ 1 console.table is use to print multiple things together in the form of table
+ 2 VAR is not used in java script moslty because of scope problem 
+*/

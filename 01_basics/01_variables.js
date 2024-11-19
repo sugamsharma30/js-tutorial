8cb52a252229c68eaffc37acbc1d9406da9316a0
sugamsharma30 <168557566+sugamsharma30@users.noreply.github.com>
2024-11-19 18:21:01 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,14 +2,15 @@ const acount =  123445 // these constants which cant be changed
 let email = "sugam@.com"
 var pas = "23456"
 city = "jaipur"
-let state // it not a good way to declare variable but it will also run    
+let state // it not a good way to declare variable but it will also run , in output it will give undefined because value are not declared   
 // acount = 2 it is not allowed the variables declare after const cant be changed 
  
 email = "hahgr@acount.com"
 pas = "3456"
 city = "delhi"
-console.table([email , pas , city ])
+console.table([email , pas , city, state ])
 /*
  1 console.table is use to print multiple things together in the form of table
- 2 VAR is not used in java script moslty because of scope problem 
+ 2 prefer not  to use VAR in java script moslty because of scope problem 
+ and functional problem
 */
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version 
+// aler(3+3) it will give error as there is a different syntax to use alert in node js
+
+let name = "sugam" //string data type 
+let age = 215 // int datatype 
+let isLoggedIn = true // boolean data type 
+//     DATA TYPES :- 
+    //primtive datatype:-
+// number = 2 to power 53 
+// bigint 
+// string 
+// boolean = ture/ false 
+// null it a data type and a stand alone value 
+// Uundefined when value is not defined 
+// symbol to find unquiness 
+ console.log(typeof age)
+ console.log(typeof null);// INTERVIEW  question type of null is obejct .
+ 

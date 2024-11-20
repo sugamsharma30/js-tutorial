39188d8705660439d16c6f79bd63620ebaf53574
sugamsharma30 <168557566+sugamsharma30@users.noreply.github.com>
2024-11-20 18:49:25 +0000

    comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+console.log("2"> 1);
+// it will give true as "2" string will automatically converted to number 2 
+
+      //**NULL**
+      console.log(null>0);// false
+      console.log(null==0)// flase 
+      console.log(null>=0);// true as it will convert null to 0 so this operation is true 
+
+        //**undefined**
+        console.log(undefined==0);
+        console.log(undefined>0);
+        console.log(undefined<0);
+        
+         //***STRICT CHECK***
+         // === it will not only check the value and also the datatype
+         console.log("2"=== 2);// it wil also check datatype and they are not same so output wil be false.
+         
+         
+
+      

5cdd1930f785b76bff03389e0fddedab0a09df3f
sugamsharma30 <168557566+sugamsharma30@users.noreply.github.com>
2024-11-20 18:02:52 +0000

    converastion of datatypes

---
+++ 01_basics/03_conversionDatatypes.js
@@ -0,0 +1,40 @@
+let score = 33
+console.log(typeof score);// output type will be number.
+let scorea = "33"
+ let scoreAB = Number(scorea)
+ console.log(typeof scorea); // output will be string  
+ 
+ console.log(typeof scoreAB);// here output will be number because we have converted the string to number
+ 
+// "33abc" = this value will also be converted to number from string but if we will print this it will give output nan that means not a number.
+// null will be  give 0 
+// common conversion 
+// "33" = 33
+// "33abcb" = NaN - not a number 
+// true = 1 , false = 0 
+  
+
+// ***boolean conversion***
+let x =  1
+let y = Boolean(x)
+console.log(x);
+console.log(y); // 1 will be converted to true 
+console.log(typeof y);
+// common cnclusions :-
+// 0 will be converted to false 
+// null will also converted to false 
+// undefined will also converted to false 
+// "" this will also converted to false 
+
+
+
+
+//    *** String converastion***
+let num = 45 
+let s = String(num)
+console.log(num)
+
+console.log(typeof s);
+console.log(s);
+
+

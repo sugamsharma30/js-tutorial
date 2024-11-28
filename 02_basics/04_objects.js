fbac225bc9937a6c3d8a235ba78626924c541983
sugamsharma30 <168557566+sugamsharma30@users.noreply.github.com>
2024-11-28 18:38:44 +0000

    object oart 2 and api

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -25,8 +25,47 @@ console.log(x);
 // to combine objects
 const y = {1:"a",2:"b"}
 const z = {3:"c",4:"d"}
-const i = Object.assign(y,z)// objects combine ho jayenge 
+const a = {6:"o",7:"p"}
+const i = Object.assign(y,z,a)// objects combine ho jayenge 
 console.log(i);
+console.log(y);// isme bhi sbki values print hongi "z","a" kyuki y ko target man lia
+console.log(z);
+const l = Object.assign({},y,z,a)// target y ko ni mana jayega jo starting me curly braces usko mana jayega
+console.log(l);
+
+
+//spread to another way to add two different objects
+const h = {...y,...z,...a}// simpliest way to add 2 or more array
+console.log(h);
+
+console.log(Object.keys(user));// sari keys ayegi jo object user tha uski 
+console.log(Object.values(user))// sari values a jayengi  object user ki
+
+// destructuring
+const course = {
+    coursename:"jai hind",
+    price:989,
+    courseInstructor:"hitesh"
+}
+// another way to access inside object
+const{courseInstructor}=course
+console.log(courseInstructor);
+console.log(course.courseInstructor);// is se courseinstructor ke andr jo h print ho jayega
+
+//API = apna kam kisi aur ke sir pe dalna is API
+// json = object jese bnate h same vese bs usko nam mt do 
+//{ //key aur values dono string me deni padti 
+//"name":"tmatar",
+//"age":"456"
+//}
+//API = kbhi array ke form me bhi milti h array jisme bhot sare objects h 
+[
+    {},
+    {},
+    {}
+]
+
+
 
 
 

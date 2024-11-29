3f9afadfbfc3845075154bff43b432e9accbcfa5
sugamsharma30 <168557566+sugamsharma30@users.noreply.github.com>
2024-11-29 20:39:54 +0000

    immediately invoked function

---
+++ 03_basics/04_IIFE.js
@@ -0,0 +1,4 @@
+// IMMEDIATELY INVOKED FUNCTION EXPRESSIONS(IIFE)
+// function that is executed immeditely
+// we use iife because global scope se jo pollution hoti h kayi bar to jo bhi global scope ke variables h usko vha se htane ke liye 
+  

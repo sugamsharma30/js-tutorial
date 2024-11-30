073ad1b87b8f38c466e1176973e7e02c0da0a180
sugamsharma30 <168557566+sugamsharma30@users.noreply.github.com>
2024-11-30 17:04:35 +0000

    IMMEDIATELY INVOKED FUNCTION EXPRESSIONS

--- 03_basics/04_IIFE.js
+++ 03_basics/04_IIFE.js
@@ -1,4 +1,15 @@
 // IMMEDIATELY INVOKED FUNCTION EXPRESSIONS(IIFE)
 // function that is executed immeditely
 // we use iife because global scope se jo pollution hoti h kayi bar to jo bhi global scope ke variables h usko vha se htane ke liye 
-  
+(function chai(){// ye ek named iife h jiska nam h chai
+    console.log(`db connected`);
+    
+})();// ye bn gya iife function , isme semi collons dene pdenge agr iske bad dusra function call krna h to 
+// global scope ke pollution se problem hoti kayi bar to vha ke variables ko htane ke liye hmne iife k use kia 
+
+// arrow functions as iife
+((name) =>{
+    console.log(`db connected two ${name}`);
+    
+})('sugam')
+// IMPORTANT 2 iife sath likhne h to phele walle iife ko khtm krne ke bad semicolon aygega

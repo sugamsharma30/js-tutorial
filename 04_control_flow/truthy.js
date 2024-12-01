6a2cd70ed5bba97c2d9821586a2ab79b5cf18f5b
sugamsharma30 <168557566+sugamsharma30@users.noreply.github.com>
2024-12-01 15:33:28 +0000

    truthy falsee

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,20 @@
+// truhty and falseee value
+//truthy value 
+const userEmail = "sugamsharma@.com"
+if(userEmail){ // yha pe hmne true h ki false ye compare he ni kia bs man lia h true h 
+    console.log("got user email ");
+    
+}else{
+    console.log("dont have user email");
+    
+}
+
+//false value 
+const email =""
+if(email){ //yha pe hmne man lia ki value false he hogi kyuki email me koi value ni de rkhi h 
+    console.log("got user email");
+    
+}else{
+    console.log("dont have email ");
+    
+}

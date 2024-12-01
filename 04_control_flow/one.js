2b72f77117c2b0b9f7eeb64f49908014a20c7eef
sugamsharma30 <168557566+sugamsharma30@users.noreply.github.com>
2024-12-01 15:18:09 +0000

    if else

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -21,4 +21,32 @@ if(score>100){
                                    // but agr hm power ko var se store krenge to ye chl jayega kyuki var k scope global h jha mrji acces kr lo which not safe .
 
                                    //nesting
-                                   
+          const balance = 1000
+          if(balance<500){
+            console.log("less than 500");
+            
+          } else if (balance<750){
+            console.log("less than 750");
+            
+          } else if(balance<900){
+            console.log("less than 900");
+            
+          }else{
+            console.log("less than 1200");
+            
+          } 
+          
+          // and operator && if both statements are true than condtion will execute
+                let x = true
+                let y = true
+                if(x && y){
+                    console.log("bakchdoi top level ki ");
+                    
+                }
+         // or operator || if any of the one statement is true the condition will execute 
+                let z = true
+                let c = false
+                if(z || c){
+                    console.log("bakchodi km ni hogi");
+                    
+                }       

127545bbfb26998fcd074ac7631482dfdb525921
sugamsharma30 <168557566+sugamsharma30@users.noreply.github.com>
2024-12-01 15:23:59 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,19 @@
+// switch
+// basci syntax 
+const month = 3
+switch (month) {// jiski value dhund re h usko dalenge keyt me 
+    case 1:
+        console.log("january"); //to copy same downwards press shift+alt+downwardkey
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+
+    default:
+        console.log("deafault");
+        
+        break;
+}

a1783fd6691573d12495d42217a3325ec1c0a785
sugamsharma30 <168557566+sugamsharma30@users.noreply.github.com>
2024-12-03 16:35:01 +0000

    truthy and flase

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -18,3 +18,40 @@ if(email){ //yha pe hmne man lia ki value false he hogi kyuki email me koi value
     console.log("dont have email ");
     
 }
+/*FALSY VALUES 
+false , o , -0 , bingInt 0n , "" , null , undefined , nan
+
+***TRUTHY VALUES
+true , "0" ,(string ke andr 0 ), "false"(string ke andr koi bhi value truthy values h),
+[] , {}(empty object bhi ),function(){}(empty function also truthy values)
+
+
+false == 0 // output true 
+false == '' // output true 
+0 == '' //output true 
+*/ 
+
+// NULLISH COALESHING OPERATOR ?? : null undefined 
+let val;
+//val = 5 ?? 10 // iska output ayega 5 kyuki phele 5 h 
+//val = null ?? 10 // ye krta h agr output null aye to uska safetty check kr de , iska output ayega 10 
+//val = undefined ?? 15 // output 15 
+// ?? ye sirf null aur undefined ke liye bna h 
+val = null ?? undefined
+console.log(val);
+//In JavaScript, the nullish coalescing operator (??)
+// is used to provide a fallback value when dealing with null or undefined. 
+//It returns the right-hand operand if the left-hand operand is null or undefined; 
+//otherwise, it returns the left-hand
+
+               // SIMPLE WORDS ?? working
+// simply it will check left side operator if it is null or undefined 
+//it wil return right side value no matter what the right side value is 
+
+
+
+// TERNARY OPERATOR
+// condition ? true : false 
+
+2 === 3 ? console.log("sugam") : console.log("harsh");
+

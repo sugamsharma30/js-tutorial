f0783caf1ef69cf7b91b2e214e6c5b83e7d426f4
sugamsharma30 <168557566+sugamsharma30@users.noreply.github.com>
2024-12-03 20:16:20 +0000

    for loop

---
+++ 05_iterations(loops)/one.js
@@ -0,0 +1,147 @@
+// for loop 
+
+for (let i = 0; i <= 10; i++) {
+    if(i == 5){
+        console.log("5 is best number ");
+        
+    }
+    console.log(i);
+    
+    
+}
+//console.log(element);// iska access for loop ke bhar ni h 
+for (let i = 0; i <=10; i++) {
+    console.log(`outter loop ${i}`);
+    for (let j = 0; j <=10; j++) {
+        console.log(`inner loop ${j} and ouuter loop ${i}`);
+}
+    }
+
+// Array loop :-
+    let Array = [ 1 , 2, 3,   4 , 5 ,6, 7 ]
+    for (let index = 0; index < Array.length; index++) {
+        const element = Array[index];
+        console.log(element);
+        
+        
+    }
+    console.log(Array.length);
+
+// break and continue 
+for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+        console.log("detected 5");
+        break // break ke bad kuch execute ni hoga and loop se bhar jayenge 
+        
+    }
+    console.log(index);
+    
+    
+for (let index = 1; index <= 20; index++) {
+    
+    if(index == 5){
+        console.log("detected 5");
+        break // break ke bad kuch execute ni hoga and loop se bhar jayenge 
+        
+    }
+    console.log(index);
+    
+    
+}    
+for (let index = 1; index <= 20; index++) {
+
+    if(index == 5){
+        console.log("detected 5");
+        break // break ke bad kuch execute ni hoga and loop se bhar jayenge 
+        
+    }
+    console.log(index);
+    
+    
+}    
+    
+for (let index = 1; index <= 20; index++) {
+
+    if(index == 5){
+        console.log("detected 5");
+        break // break ke bad kuch execute ni hoga and loop se bhar jayenge 
+        
+    }
+    console.log(index);
+    
+    
+}    
+    
+for (let index = 1; index <= 20; index++) {
+
+    if(index == 5){
+        console.log("detected 5");
+        break // break ke bad kuch execute ni hoga and loop se bhar jayenge 
+        
+    }
+    console.log(index);
+    
+    
+}    
+    
+for (let index = 1; index <= 20; index++) {
+
+    if(index == 5){
+        console.log("detected 5");
+        break // break ke bad kuch execute ni hoga and loop se bhar jayenge 
+        
+    }
+    console.log(index);
+    
+    
+}    
+    
+for (let index = 1; index <= 20; index++) {
+
+
+    if(index == 5){
+        console.log("detected 5");
+        break // break ke bad kuch execute ni hoga and loop se bhar jayenge 
+        
+    }
+    console.log(index);
+    
+    
+}    
+    
+for (let index = 1; index <= 20; index++) {
+
+    if(index == 5){
+        console.log("detected 5");
+        break // break ke bad kuch execute ni hoga and loop se bhar jayenge 
+        
+    }
+    console.log(index);
+    
+    
+}    
+    
+for (let index = 1; index <= 20; index++) {
+
+    if(index == 5){
+        console.log("detected 5");
+        break // break ke bad kuch execute ni hoga and loop se bhar jayenge 
+        
+    }
+    console.log(index);
+
+   
+    
+}}
+
+for (let k = 0; k <= 20; k++) {
+    if(k==5){
+        console.log("5 mil gya");
+        continue // bs 5 ko print ki krega ek bari skip krega uske bad continue kr dega 
+    }
+    console.log(k);
+    
+    
+}
+
+    

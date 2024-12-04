a6b1009cc761e0d8877abe72188ec1362a9603bd
sugamsharma30 <168557566+sugamsharma30@users.noreply.github.com>
2024-12-04 11:06:47 +0000

    while do while loop

---
+++ 05_iterations(loops)/two.js
@@ -0,0 +1,27 @@
+// while and do while loop 
+ let x = 2 // intilization , condition check and than increment or decrement 
+while (x <= 20) {
+    console.log(x);
+    x+=2
+    
+    
+}
+let Array = [1 ,2 ,3,4 , 6 ]
+let y = 0
+while (y<Array.length) {
+    console.log(Array);
+    Array++
+    console.log(Array.length);
+    
+    
+
+    
+}
+// do while loop = phele statemnt check hogi than conditon check hogi 
+let z = 8
+do {
+    console.log(z);
+    z+=8
+} while (z<=5); // yha pe condtion satisfy ni hori thi tbhi ek bari "DO" block ke andr jo likha h wo chla 
+       
+
